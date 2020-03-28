a93e5702d47507c04cfb0923d8cda1b151d3ab82
Brentwood Reid <bwoodreid@gmail.com>
2020-03-27 20:06:08 -0700

    Set default route for action_mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,7 @@ Rails.application.configure do
                         }
 
    # Setup the mailer config
+  Rails.application.routes.default_url_options[:host] = 'https://ak-team-pios.herokuapp.com/'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
